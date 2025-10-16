8804ebeae427d5cebab4724b881a6e22a0fa49ee
myisamkernel <138276597+myisamkernel@users.noreply.github.com>
2025-10-17 03:32:30 +0700

    small change

--- src/server.js
+++ src/server.js
@@ -1,7 +1,12 @@
 const express = require("express");
 
 const app = express();
+const port = 3000
 
 require("./routes/user")(app)
 
 app.use(express.json());
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
