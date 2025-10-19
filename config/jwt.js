a65642bf2d31ec60b923c397d3063fc58b7f4ed4
myisamkernel <138276597+myisamkernel@users.noreply.github.com>
2025-10-19 20:07:38 +0700

    rbac and jwt polishment

--- config/jwt.js
+++ config/jwt.js
@@ -1,5 +1,5 @@
-const JWT_SECRET = "test1234";
-const JWT_EXPIRES_IN = "1h"; // Access token expires in 1 hour
+const JWT_SECRET = process.env.JWT_SECRET;
+const JWT_EXPIRES_IN = process.env.JWT_EXPIRES_IN; // Access token expires in 1 hour
 
 module.exports = {
   JWT_SECRET,
--- server.js
+++ server.js
@@ -6,8 +6,8 @@ const port = 3000
 
 app.use(express.json());
 
-require("./routes/user")(app)
-require("./routes/dataRoute")(app)
+require("./src/routes/user")(app)
+require("./src/routes/dataRoute")(app)
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
