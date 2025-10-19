cabf16d94611c218678756618aa323cd3eca5299
myisamkernel <138276597+myisamkernel@users.noreply.github.com>
2025-10-19 19:09:33 +0700

    rbac and jwt polishment

--- src/routes/dataRoute.js
+++ src/routes/dataRoute.js
@@ -2,17 +2,18 @@ const {
   getListData,
   storeData,
   deleteData,
+  editData,
 } = require("../controllers/data");
 const { authorizeAbac } = require("../middlewares/abac");
 const { authenticateToken } = require("../middlewares/jwtMiddleware");
 const { authorizeRole } = require("../middlewares/rbac");
 
 module.exports = (app) => {
-  app.get("/data", authenticateToken, authorizeAbac, getListData);
+  app.get("/data", authenticateToken, authorizeRole("read"), getListData);
 
   app.post("/data", authenticateToken, authorizeRole("write"), storeData);
 
-  app.put("/data", authenticateToken, authorizeRole("write"), storeData);
+  app.put("/data", authenticateToken, authorizeRole("write"), editData);
 
   app.delete(
     "/data/:id",
